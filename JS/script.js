8bebdbc9466af9464a7ca4d134476f09a58dfd77
Reinaldo Gonçalves <reinaldo110118@gmail.com>
2022-01-02 12:23:17 -0300

    commit inicial

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+document.getElementById('gray').addEventListener("click",function(){
+    document.querySelector("body").setAttribute("id","gray");
+});
+
+document.getElementById('blue').addEventListener("click",function(){
+    document.querySelector("body").setAttribute("id","blue");
+});
+
+document.getElementById('green').addEventListener("click",function(){
+    document.querySelector("body").setAttribute("id","green");
+});
+
+document.getElementById('red').addEventListener("click",function(){
+    document.querySelector("body").setAttribute("id","red");
+});
+
+document.getElementById('yellow').addEventListener("click",function(){
+    document.querySelector("body").setAttribute("id","yellow");
+});
+
+document.getElementById('orange').addEventListener("click",function(){
+    document.querySelector("body").setAttribute("id","orange");
+});
